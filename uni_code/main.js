9582b765d86e522aa104c99961d86353461dde58
kukela <kukela@sina.cn>
2024-02-07 19:01:23 +0800

    项目骨架搭建

--- uni_code/main.js
+++ uni_code/main.js
@@ -6,17 +6,35 @@ import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
+
+uni.setNavStyle = () => {
+	const theme = uni.getTheme()
+	// App端
+	// #ifdef APP-PLUS
+	plus.navigator.setStatusBarStyle(theme === 'dark' ? 'light' : 'dark') // 只支持dark和light
+	// #endif
+
+	// 小程序端
+	// #ifdef MP-WEIXIN
+	uni.setNavigationBarColor({
+		frontColor: theme === 'dark' ? '#fff' : '#000',
+		backgroundColor: theme === 'dark' ? '#000' : '#fff'
+	})
+	// #endif
+}
--- uni_code/pages/index/index.vue
+++ uni_code/pages/index/index.vue
@@ -1,55 +1,133 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="box">
+		<view class="cxt_box">
+			<view class="header_box">
+				<view class="num_box">
+					<text class="num">预算:3000</text>
+					<text class="num big">剩余:1000</text>
+				</view>
+				<view class="all_bar">
+					<view class="h_bar"></view>
+				</view>
+			</view>
+			<view class="cell"></view>
+			<view class="space"></view>
+			<view class="space"></view>
+			<view class="space"></view>
+			<view class="space"></view>
+			<view class="space"></view>
+			<view class="space"></view>
 		</view>
+
+		<movable-area class="bottom_bar">
+			<movable-view class="bottom_bar_box">
+				<view class="add_btn" hover-class="add_btn_hover" hover-stay-time="150">添 加</view>
+			</movable-view>
+		</movable-area>
 	</view>
 </template>
 
 <script>
-	
-	export default {
-		data() {
-			return {
-				title: 'Hello'
+export default {
+	data() {
+		return {
+			title: '本月',
+			pattern: {
+				buttonColor: '@submit_bg_color',
+				iconColor: '@submit_text_color'
 			}
-		},
-		onLoad() {
-			
-			console.log("-----")
-		
-		},
-		methods: {
+		};
+	},
+	onLoad() {
+		uni.getSystemInfo({
+			success: function (res) {
+				console.log(res);
+			}
+		});
 
+		console.log('-2----');
+	},
+	methods: {
+		trigger(e) {
+			console.log(e);
 		}
 	}
+};
 </script>
 
-<style>
-	.content {
+<style lang="scss" scoped>
+@import url(../global/global.scss);
+
+.cxt_box {
+	display: flex;
+	flex-direction: column;
+	position: relative;
+	padding: 40rpx 40rpx 150rpx 40rpx;
+}
+
+.header_box {
+	height: 160rpx;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+
+	.num_box {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
+		justify-content: space-between;
 		align-items: center;
-		justify-content: center;
+
+		.num {
+			font-size: 24rpx;
+			color: #7d90a9;
+		}
+
+		.num.big {
+			font-size: 32rpx;
+		}
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+	.all_bar {
+		background-color: #292929;
+		border-radius: 10rpx;
+		height: 40rpx;
+		width: 100%;
+		font-size: 0;
+		line-height: 0;
+		margin: 20rpx 0;
 
-	.text-area {
-		display: flex;
-		justify-content: center;
+		.h_bar {
+			background-color: #74a800;
+			width: 40%;
+			height: 30rpx;
+			margin: 5rpx;
+			border-radius: 10rpx;
+			display: inline-block;
+		}
 	}
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.space {
+	height: 100rpx;
+	margin: 20rpx 0;
+	background-color: #292929;
+	border-radius: 20rpx;
+}
+
+.add_btn {
+	width: 100%;
+	height: 90rpx;
+	background-color: #0e0d0d;
+	color: #8183ff;
+	text-align: center;
+	line-height: 90rpx;
+	border-radius: 20rpx;
+	font-size: 46rpx;
+	font-weight: bold;
+}
+
+.add_btn_hover {
+	background-color: #1d1b1b;
+	color: #989fff;
+}
 </style>
